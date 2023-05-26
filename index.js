d7c5954ae75fea2f33703ec4d7a4f060d5118776
Evan Edwards <evan@evanville.com>
2023-05-25 22:16:52 -0400

    add dependancies, clients, exchange rates, and tax rates

--- index.js
+++ index.js
@@ -4,39 +4,137 @@ const csv = require('csvtojson');
 const { default: axios } = require('axios');
 const limiter = new RateLimiter({ tokensPerInterval: 9, interval: 2000 });
 
+//Promises for dependancies, as needed
+const dependancies = {
+    taxrateheader: resolver(),
+    exchangeheader: resolver(),
+};
+
 //Define the workflows here, one per endpoint/csv combo
 const workflows = [
     {   
+        enabled: false,
         description: 'load base transactions from csv to lb',
         data: async () => {
             return csv().fromFile(process.env.BASE_TRANS_CSV || './basetrans.csv')
         },
         action: async (doc) => {
-            await csvToJsonToPost(`${process.env.LB_URL}${process.env.BASE_TRANS_URL}`, doc)
+            //Note: Can do remapping right here if needed if the CSV columns don't match the JSON LB model
+            await csvToJsonToAPI('post', `${process.env.LB_URL}${process.env.BASE_TRANS_URL || '/transdata/v1/transaction/base'}`, doc)
+        }
+    },
+    {   
+        enabled: false,
+        description: 'load tax rate headers from csv to lb',
+        data: async () => {
+            return csv().fromFile(process.env.TAX_HEADER_CSV || './taxheaders.csv')
+        },
+        action: async (doc) => {
+            const mapped = { dataList: [{ data: doc }] };
+            await csvToJsonToAPI('post', `${process.env.LB_URL}${process.env.TAX_RATE_HEADER_URL || '/contract/v1/mqs/taxrateheader'}`, mapped)
+        },
+        complete: dependancies.taxrateheader.resolver
+    },
+    {   
+        enabled: false,
+        description: 'load tax rate details from csv to lb',
+        data: async () => {
+            return csv().fromFile(process.env.TAX_DTL_CSV || './taxdetails.csv')
+        },
+        action: async (doc) => {
+            const id = await getID('post', `${process.env.LB_URL}${process.env.TAX_RATE_HEADER_URL || '/contract/v1/mqs/taxrateheader'}/query?size=1&page=0`, [{key: 'taxRateId', value: doc.headerKey, operation: 'eq'}])
+            const mapped = { dataList: [{ data: {...doc, headerKey: id.response[0].rateKey} }] };
+            await csvToJsonToAPI('post', `${process.env.LB_URL}${process.env.TAX_RATE_DETAIL_URL || '/contract/v1/mqs/taxratedetails'}`, mapped)
+        },
+        depends: dependancies.taxrateheader.promise
+    },
+    {   
+        enabled: false,
+        description: 'load exchange rate headers from csv to lb',
+        data: async () => {
+            return csv().fromFile(process.env.EXCHANGE_RATE_HEADER_CSV || './exchangeheaders.csv')
+        },
+        action: async (doc) => {
+            const mapped = { dataList: [{ data: doc }] };
+            await csvToJsonToAPI('post', `${process.env.LB_URL}${process.env.EXCHANGE_RATE_HEADER_URL || '/contract/v1/mqs/exchangeRateHeader'}`, mapped)
+        },
+        complete: dependancies.exchangeheader.resolver
+    },
+    {   
+        enabled: false,
+        description: 'load exchange rate details from csv to lb',
+        data: async () => {
+            return csv().fromFile(process.env.EXCHANGE_RATE_DTL_CSV || './exchangedetails.csv')
+        },
+        action: async (doc) => {
+            const id = await getID('post', `${process.env.LB_URL}${process.env.EXCHANGE_RATE_HEADER_URL || '/contract/v1/mqs/exchangeRateHeader'}/query?size=1&page=0`, [{key: 'exchangeRateId', value: doc.headerKey, operation: 'eq'}])
+            const mapped = { dataList: [{ data: {...doc, headerKey: id.response[0].headerKey} }] };
+            await csvToJsonToAPI('post', `${process.env.LB_URL}${process.env.EXCHANGE_RATE_DETAIL_URL || '/contract/v1/mqs/exchangeRateDetails'}`, mapped)
+        },
+        depends: dependancies.exchangeheader.promise
+    },
+
+    {   
+        enabled: false,
+        description: 'load clients from csv to lb',
+        data: async () => {
+            return csv().fromFile(process.env.CLIENT_CSV || './client.csv');
+        },
+        action: async (doc) => {
+            const mapped = { dataList: [{ data: doc }] };
+            await csvToJsonToAPI('post', `${process.env.LB_URL}${process.env.CLIENT_URL || '/contract/v1/mqs/client'}`, mapped);
         }
     },
     //Add next workflow here, like for contracts or something
 ];
 
-//Generic csv to json with post
-async function csvToJsonToPost(url, doc){
+//Lookup ID for header to insert details
+async function getID(method, url, data){
     await limiter.removeTokens(1);
-    log('basetrans', 'info', 'action triggered');
-    //TODO: validate the doc and skip if invalid
-    axios.post(url, doc, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + process.env.AUTH_TOKEN
-        }
-    }).then((res) => {
-        log('basetrans', 'info', 'action completed', res.status, res.data);
-    }).catch((err) => {
-        log('basetrans', 'error', 'action failed', err.message, err.response.data);
+    log('getID', 'info', 'lookuping up id for key');
+    try {
+        const res = await axios({ method, url, data,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + process.env.AUTH_TOKEN
+            }
+        })
+        log('getID', 'info', 'lookup completed', res.status, res.data);
+        return res.data;
+    } catch(err) {
+        log('getID', 'error', 'lookup failed', err.message, err.response.data);
         if(err.response.status === 401){
-            log('basetrans', 'error', 'skip remaining data due to unauthorized', err.message, err.response.data);
+            log('getID', 'error', 'skip remaining data due to unauthorized', err.message, err.response.data);
             throw new Error('skip remaining data due to unauthorized');
         }
-    });
+    }
+}
+
+//Generic csv to json with post
+async function csvToJsonToAPI(method, url, data){
+    await limiter.removeTokens(1);
+    log('csvToJsonToAPI', 'info', 'action triggered');
+    try {
+        const res = await axios({ method, url, data,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + process.env.AUTH_TOKEN
+            }
+        })
+        log('csvToJsonToAPI', 'info', 'action completed', res.status, res.data);
+    } catch(err) {
+        log('csvToJsonToAPI', 'error', 'action failed', err.message, err.response.data);
+        if(err.response.status === 401){
+            log('csvToJsonToAPI', 'error', 'skip remaining data due to unauthorized', err.message, err.response.data);
+            throw new Error('skip remaining data due to unauthorized');
+        }
+    }
+}
+
+//Generic promise to wait for a dependancy
+function resolver(){
+    let resolver;
+    return { promise: new Promise((resolve, reject) => { resolver = resolve }), resolver };
 }
 
 //Generic logging with on/off switch
@@ -52,20 +150,30 @@ async function execute(){
     log('controller', 'info', 'workflows jobs are initializing')
     await Promise.allSettled(workflows.map(async (workflow) => {
         log('controller', 'info', `Running job`, workflow.description)
-        if(workflow?.data !== undefined && workflow?.action !== undefined){
+        if(workflow?.data !== undefined && workflow?.action !== undefined && workflow.enabled === true){
             log('controller', 'info', 'workflow has data and action, running', workflow.description)
-            workflow
-                .data()
-                .then((row) => {
-                    row.map(async (doc) => {
-                        log('controller', 'info', 'job record found', workflow.description, doc)
-                        await workflow.action(doc);
-                        log('controller', 'info', 'job record processed', workflow.description)
-                    });
-                })
-                .catch(err => {
-                    log('controller', 'error', 'job failed', workflow.description, err.message)
-                });
+            if(workflow.depends !== undefined){
+                log('controller', 'info', 'workflow has dependancy, waiting', workflow.description)
+                await workflow.depends;
+            }
+            try {
+                const data = await workflow.data()
+                await Promise.allSettled(data.map(async (doc) => {
+                    log('controller', 'info', 'job record found', workflow.description, doc)
+                    await workflow.action(doc);
+                    log('controller', 'info', 'job record processed', workflow.description)
+                }));
+            } catch (err) {
+                log('controller', 'error', 'job failed', workflow.description, err.message)
+            } finally {
+                if(workflow.complete !== undefined){
+                    workflow.complete();
+                    log('controller', 'info', 'job complete, releasing dependant jobs', workflow.description)
+                }
+            }
+        }else if(workflow.enabled === false && workflow.complete !== undefined){
+            workflow.complete();
+            log('controller', 'info', 'disabled job complete, releasing dependant jobs', workflow.description)
         }
     }));
 }
